69524203659c58e9ffc5627c1e9ebe12496a2664
CatMao1230 <cat841230@gmail.com>
2017-02-26 22:46:57 +0800

    commit

---
+++ Big Dipper/1032002/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace _1032002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxSize = new System.Windows.Forms.TextBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxScale = new System.Windows.Forms.TextBox();
+            this.btnDown2 = new System.Windows.Forms.Button();
+            this.btnUp2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDown3 = new System.Windows.Forms.Button();
+            this.btnUp3 = new System.Windows.Forms.Button();
+            this.btnRight3 = new System.Windows.Forms.Button();
+            this.btnLeft3 = new System.Windows.Forms.Button();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(727, 553);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(806, 22);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(53, 35);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Off";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(753, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "連線";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "大小";
+            // 
+            // tbxSize
+            // 
+            this.tbxSize.Location = new System.Drawing.Point(806, 86);
+            this.tbxSize.Name = "tbxSize";
+            this.tbxSize.ReadOnly = true;
+            this.tbxSize.Size = new System.Drawing.Size(53, 25);
+            this.tbxSize.TabIndex = 4;
+            this.tbxSize.Text = "5";
+            this.tbxSize.TextChanged += new System.EventHandler(this.tbxSize_TextChanged);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(756, 125);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(44, 35);
+            this.btnUp.TabIndex = 5;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(815, 125);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(44, 35);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.Text = "↓";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(753, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "縮放";
+            // 
+            // tbxScale
+            // 
+            this.tbxScale.Location = new System.Drawing.Point(806, 187);
+            this.tbxScale.Name = "tbxScale";
+            this.tbxScale.ReadOnly = true;
+            this.tbxScale.Size = new System.Drawing.Size(53, 25);
+            this.tbxScale.TabIndex = 8;
+            this.tbxScale.Text = "1";
+            this.tbxScale.TextChanged += new System.EventHandler(this.tbxScale_TextChanged);
+            // 
+            // btnDown2
+            // 
+            this.btnDown2.Location = new System.Drawing.Point(815, 226);
+            this.btnDown2.Name = "btnDown2";
+            this.btnDown2.Size = new System.Drawing.Size(44, 35);
+            this.btnDown2.TabIndex = 10;
+            this.btnDown2.Text = "↓";
+            this.btnDown2.UseVisualStyleBackColor = true;
+            this.btnDown2.Click += new System.EventHandler(this.btnDown2_Click);
+            // 
+            // btnUp2
+            // 
+            this.btnUp2.Location = new System.Drawing.Point(756, 226);
+            this.btnUp2.Name = "btnUp2";
+            this.btnUp2.Size = new System.Drawing.Size(44, 35);
+            this.btnUp2.TabIndex = 9;
+            this.btnUp2.Text = "↑";
+            this.btnUp2.UseVisualStyleBackColor = true;
+            this.btnUp2.Click += new System.EventHandler(this.btnUp2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(753, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "平移";
+            // 
+            // btnDown3
+            // 
+            this.btnDown3.Location = new System.Drawing.Point(785, 392);
+            this.btnDown3.Name = "btnDown3";
+            this.btnDown3.Size = new System.Drawing.Size(44, 35);
+            this.btnDown3.TabIndex = 13;
+            this.btnDown3.Text = "↓";
+            this.btnDown3.UseVisualStyleBackColor = true;
+            this.btnDown3.Click += new System.EventHandler(this.btnDown3_Click);
+            // 
+            // btnUp3
+            // 
+            this.btnUp3.Location = new System.Drawing.Point(785, 310);
+            this.btnUp3.Name = "btnUp3";
+            this.btnUp3.Size = new System.Drawing.Size(44, 35);
+            this.btnUp3.TabIndex = 12;
+            this.btnUp3.Text = "↑";
+            this.btnUp3.UseVisualStyleBackColor = true;
+            this.btnUp3.Click += new System.EventHandler(this.btnUp3_Click);
+            // 
+            // btnRight3
+            // 
+            this.btnRight3.Location = new System.Drawing.Point(815, 351);
+            this.btnRight3.Name = "btnRight3";
+            this.btnRight3.Size = new System.Drawing.Size(44, 35);
+            this.btnRight3.TabIndex = 15;
+            this.btnRight3.Text = "→";
+            this.btnRight3.UseVisualStyleBackColor = true;
+            this.btnRight3.Click += new System.EventHandler(this.btnRight3_Click);
+            // 
+            // btnLeft3
+            // 
+            this.btnLeft3.Location = new System.Drawing.Point(756, 351);
+            this.btnLeft3.Name = "btnLeft3";
+            this.btnLeft3.Size = new System.Drawing.Size(44, 35);
+            this.btnLeft3.TabIndex = 14;
+            this.btnLeft3.Text = "←";
+            this.btnLeft3.UseVisualStyleBackColor = true;
+            this.btnLeft3.Click += new System.EventHandler(this.btnLeft3_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(806, 445);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(53, 35);
+            this.btnRotate.TabIndex = 16;
+            this.btnRotate.Text = "Off";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(753, 455);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "旋轉";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(756, 501);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(103, 35);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRotate);
+            this.Controls.Add(this.btnRight3);
+            this.Controls.Add(this.btnLeft3);
+            this.Controls.Add(this.btnDown3);
+            this.Controls.Add(this.btnUp3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDown2);
+            this.Controls.Add(this.btnUp2);
+            this.Controls.Add(this.tbxScale);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.tbxSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.MaximumSize = new System.Drawing.Size(900, 600);
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "北斗七星";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxSize;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxScale;
+        private System.Windows.Forms.Button btnDown2;
+        private System.Windows.Forms.Button btnUp2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDown3;
+        private System.Windows.Forms.Button btnUp3;
+        private System.Windows.Forms.Button btnRight3;
+        private System.Windows.Forms.Button btnLeft3;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Big Dipper/1032002/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tao.OpenGl; // 引入 Tao.OpenGl
+
+namespace _1032002
+{
+    public partial class Form1 : Form
+    {
+        bool DrawLine = false;
+        bool Rotate = false;
+        float PointSize = 5;
+        float ScaleSize = 1;
+        int MoveX = 0;
+        int MoveY = 0;
+        int RotateAngle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.simpleOpenGlControl1.InitializeContexts(); // 初始化
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RePaint() // 重新繪圖
+        {
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1.0f, 1.0f, 1.0f); // 白
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            // 設定繪圖的範圍 left right bottom top 左下為(0, 0) 往上為Y 往右為X
+            this.simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // "設定"清除畫面的顏色 R G B Alpha
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION); // 告知系統目前要更改的是投影矩陣
+            Gl.glLoadIdentity(); // 將目前矩陣初始化
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            // 設定繪圖的範圍 left right bottom top 左下為(0, 0) 往上為Y 往右為X
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Random rn = new Random(1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
+            Gl.glPointSize(1.0f);
+            Gl.glBegin(Gl.GL_POINTS);
+            for (int i = 0; i < 200; i++)
+            {
+                Gl.glVertex2i(rn.Next(0, this.simpleOpenGlControl1.Size.Width),
+                              rn.Next(0, this.simpleOpenGlControl1.Size.Height));
+            }
+            Gl.glVertex2i(50, 50);
+            Gl.glEnd();
+
+
+            Gl.glTranslatef(MoveX, MoveY, 0); // 平移矩陣
+            Gl.glScalef(ScaleSize, ScaleSize, ScaleSize); // 縮放矩陣
+            Gl.glRotatef(RotateAngle, 0.0f, 0.0f, 1.0f); // 旋轉矩陣
+            // 右手大拇指指向（0，0，0）至（0，0，1）的方向，四個手指的彎曲指向即是旋轉方向。
+
+            if (DrawLine)
+            {
+                // glLineWidth(width); width為float，在0~10.0，大於10以上按10處理。
+                Gl.glLineWidth(PointSize);
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+            }
+            else
+            {
+                Gl.glPointSize(PointSize);
+                Gl.glBegin(Gl.GL_POINTS);
+            }
+            // 北斗七星
+
+            Gl.glColor3f(1.0f, 0.0f, 0.0f); // 紅
+            Gl.glVertex2i(100, 250);
+            Gl.glColor3f(1.0f, 0.5f, 0.0f); // 橙
+            Gl.glVertex2i(134, 320);
+            Gl.glColor3f(1.0f, 1.0f, 0.0f); // 黃
+            Gl.glVertex2i(249, 293);
+            Gl.glColor3f(0.0f, 1.0f, 0.0f); // 綠
+            Gl.glVertex2i(255, 229);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f); // 藍
+            Gl.glVertex2i(321, 182);
+            Gl.glColor3f(0.5f, 0.0f, 1.0f); // 靛
+            Gl.glVertex2i(369, 140);
+            Gl.glColor3f(1.0f, 0.0f, 1.0f); // 紫
+            Gl.glVertex2i(469, 140);
+
+            Gl.glEnd();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e) // 畫線
+        {
+            if (DrawLine)
+            {
+                btnDraw.Text = "Off";
+                DrawLine = false;
+
+                RePaint();
+            }
+            else
+            {
+                btnDraw.Text = "On";
+                DrawLine = true;
+
+                RePaint();
+            }
+        }
+
+        private void tbxSize_TextChanged(object sender, EventArgs e) // 大小
+        {
+            RePaint();
+        }
+
+        private void btnUp_Click(object sender, EventArgs e) // 大小↑
+        {
+            PointSize = float.Parse(tbxSize.Text);
+            if (PointSize < 10.0f)
+            {
+                PointSize++;
+                tbxSize.Text = PointSize.ToString();
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e) // 大小↓
+        {
+            PointSize = float.Parse(tbxSize.Text);
+            if (PointSize > 1.0f)
+            {
+                PointSize--;
+                tbxSize.Text = PointSize.ToString();
+            }
+        }
+
+        private void btnUp2_Click(object sender, EventArgs e) // 縮放↑
+        {
+            ScaleSize = float.Parse(tbxScale.Text);
+            if (ScaleSize < 2.0f)
+            {
+                ScaleSize += 0.1f;
+                tbxScale.Text = ScaleSize.ToString();
+            }
+        }
+
+        private void btnDown2_Click(object sender, EventArgs e) // 縮放↓
+        {
+            ScaleSize = float.Parse(tbxScale.Text);
+            if (ScaleSize > 0.1f)
+            {
+                ScaleSize -= 0.1f;
+                tbxScale.Text = ScaleSize.ToString();
+            }
+        }
+
+        private void tbxScale_TextChanged(object sender, EventArgs e) // 縮放
+        {
+            ScaleSize = float.Parse(tbxScale.Text);
+            RePaint();
+        }
+
+        private void btnUp3_Click(object sender, EventArgs e) // 平移↑
+        {
+            MoveY+=5;
+            RePaint();
+        }
+
+        private void btnDown3_Click(object sender, EventArgs e) // 平移↓
+        {
+            MoveY-=5;
+            RePaint();
+        }
+
+        private void btnLeft3_Click(object sender, EventArgs e) // 平移←
+        {
+            MoveX -= 5;
+            RePaint();
+        }
+
+        private void btnRight3_Click(object sender, EventArgs e) // 平移→
+        {
+            MoveX += 5;
+            RePaint();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 旋轉計時器
+        {
+            RotateAngle ++;
+            if (RotateAngle == 360)
+            {
+                RotateAngle = 0;
+            }
+            RePaint();
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e) // 旋轉
+        {
+            if (Rotate)
+            {
+                btnRotate.Text = "Off";
+                Rotate = false;
+                timer1.Enabled = false;
+                RePaint();
+            }
+            else
+            {
+                btnRotate.Text = "On";
+                Rotate = true;
+                timer1.Enabled = true;
+                RePaint();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e) // 重置
+        {
+            DrawLine = false;
+            Rotate = false;
+            timer1.Enabled = false;
+            btnRotate.Text = "Off";
+            btnDraw.Text = "Off";
+            PointSize = 5;
+            tbxSize.Text = "5"; 
+            ScaleSize = 1;
+            tbxScale.Text = "1";
+            MoveX = 0;
+            MoveY = 0;
+            RotateAngle = 0;
+            RePaint();
+        }
+    }
+}
