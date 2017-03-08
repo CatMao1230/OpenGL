7459baf99a0d8284731819eefad37dc31d615954
CatMao1230 <CatMao1230>
2017-03-08 09:48:43 +0800

    commit2

---
+++ Trigonometric/1032002/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _1032002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxCount = new System.Windows.Forms.TextBox();
+            this.btnCountUp = new System.Windows.Forms.Button();
+            this.btnCountDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(822, 535);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.Resize += new System.EventHandler(this.simpleOpenGlControl1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(843, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "波數";
+            // 
+            // tbxCount
+            // 
+            this.tbxCount.Location = new System.Drawing.Point(886, 32);
+            this.tbxCount.Name = "tbxCount";
+            this.tbxCount.ReadOnly = true;
+            this.tbxCount.Size = new System.Drawing.Size(100, 25);
+            this.tbxCount.TabIndex = 2;
+            this.tbxCount.Text = "1";
+            this.tbxCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxCount.TextChanged += new System.EventHandler(this.tbxCount_TextChanged);
+            // 
+            // btnCountUp
+            // 
+            this.btnCountUp.Location = new System.Drawing.Point(886, 72);
+            this.btnCountUp.Name = "btnCountUp";
+            this.btnCountUp.Size = new System.Drawing.Size(46, 34);
+            this.btnCountUp.TabIndex = 3;
+            this.btnCountUp.Text = "↑";
+            this.btnCountUp.UseVisualStyleBackColor = true;
+            this.btnCountUp.Click += new System.EventHandler(this.btnCountUp_Click);
+            // 
+            // btnCountDown
+            // 
+            this.btnCountDown.Location = new System.Drawing.Point(940, 72);
+            this.btnCountDown.Name = "btnCountDown";
+            this.btnCountDown.Size = new System.Drawing.Size(46, 34);
+            this.btnCountDown.TabIndex = 4;
+            this.btnCountDown.Text = "↓";
+            this.btnCountDown.UseVisualStyleBackColor = true;
+            this.btnCountDown.Click += new System.EventHandler(this.btnCountDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 535);
+            this.Controls.Add(this.btnCountDown);
+            this.Controls.Add(this.btnCountUp);
+            this.Controls.Add(this.tbxCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.MaximumSize = new System.Drawing.Size(1034, 582);
+            this.MinimumSize = new System.Drawing.Size(1034, 582);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "三角函數";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxCount;
+        private System.Windows.Forms.Button btnCountUp;
+        private System.Windows.Forms.Button btnCountDown;
+    }
+}
+
---
+++ Trigonometric/1032002/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace _1032002
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;//計算波數
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.simpleOpenGlControl1.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+        }
+
+        private void Repaint()
+        {
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            //設定繪圖的範圍 left right bottom top 左下為(0, 0) 往上為Y 往右為X
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+            this.simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            // 背景顏色
+            Gl.glBegin(Gl.GL_QUADS);
+                //左下
+                Gl.glColor3d(0.7, 0.9, 1);
+                Gl.glVertex2i(0, 0);
+                Gl.glVertex2i(0, this.simpleOpenGlControl1.Size.Height/2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width/3, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width/3, 0);
+                //中上
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height / 2);
+                //右下
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, 0);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, 0);
+                //左上
+                Gl.glColor3d(1, 0.8, 0.8);
+                Gl.glVertex2i(0, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(0, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+                //中下
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, 0);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, 0);
+                //右上
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height / 2);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height / 2);
+            Gl.glEnd();
+
+            // 紅線
+            Gl.glBegin(Gl.GL_LINES);
+                Gl.glColor3d(1, 0, 0);
+                //—下
+                Gl.glVertex2i(0, this.simpleOpenGlControl1.Size.Height / 4);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height / 4);
+                //—上
+                Gl.glVertex2i(0, this.simpleOpenGlControl1.Size.Height / 4 * 3);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height / 4 * 3);
+                //｜左
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 6, 0);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 6, this.simpleOpenGlControl1.Size.Height);
+                //｜中
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 2, 0);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 2, this.simpleOpenGlControl1.Size.Height);
+                //｜右
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 6 * 5 + 3, 0);
+                Gl.glVertex2i(this.simpleOpenGlControl1.Size.Width / 6 * 5 + 3, this.simpleOpenGlControl1.Size.Height);
+            Gl.glEnd();
+
+            // Sin
+            Gl.glLoadIdentity();
+            Gl.glViewport(0, this.simpleOpenGlControl1.Size.Height / 2, this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+            Glu.gluOrtho2D(count * -Math.PI, count * Math.PI, -2.0f, 2.0f);
+            Gl.glColor3d(0.0, 0.0, 0.0); //black
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = count * -2 * Math.PI; x <= count * 2 * Math.PI; x += 0.005)
+                {
+                    double fx = Math.Sin(x);
+                    Gl.glVertex2d(x, fx);
+                }
+            Gl.glEnd();
+
+            // Cos
+            Gl.glLoadIdentity();
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+            Glu.gluOrtho2D(count * -Math.PI, count * Math.PI, -2.0f, 2.0f);
+            Gl.glColor3d(0.0, 0.0, 0.0); //black
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = count * -2 * Math.PI; x <= count * 2 * Math.PI; x += 0.005)
+                {
+                    double fx = Math.Cos(x);
+                    Gl.glVertex2d(x, fx);
+                }
+            Gl.glEnd();
+
+            // Tan
+            Gl.glLoadIdentity();
+            Gl.glViewport(this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2, this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+            Glu.gluOrtho2D(count * -Math.PI, count * Math.PI, -2.0f, 2.0f);
+            Gl.glColor3d(0.0, 0.0, 0.0); //black
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = count * -Math.PI; x <= count * Math.PI; x += 0.005)
+                {
+                    double fx = Math.Tan(x);
+                    if (Math.Abs(Math.Tan(x) - Math.Tan(x - 0.01)) > 0.1)
+                    {
+                        Gl.glEnd();
+                        Gl.glVertex2d(x, fx);
+                        Gl.glBegin(Gl.GL_LINE_STRIP);
+                    }
+                    else
+                    {
+                        Gl.glVertex2d(x, fx);
+                    }
+                }
+            Gl.glEnd();
+
+            // Cot
+            for (int i = 0; i < count; i++)
+            {
+                Gl.glLoadIdentity();
+                Gl.glViewport(this.simpleOpenGlControl1.Size.Width / 3 + this.simpleOpenGlControl1.Size.Width / 3 / count * i, 0, this.simpleOpenGlControl1.Size.Width / 3 / count, this.simpleOpenGlControl1.Size.Height / 2);
+                Glu.gluOrtho2D(-Math.PI, Math.PI, -2.0f, 2.0f);
+                Gl.glColor3d(0.0, 0.0, 0.0); //black
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = -Math.PI; x < 0; x += 0.005)
+                {
+                    double fx = 1 / Math.Tan(x);
+                    Gl.glVertex2d(x, fx);
+                }
+                for (double x = 0; x < Math.PI; x += 0.005)
+                {
+                    double fx = 1 / Math.Tan(x);
+                    Gl.glVertex2d(x, fx);
+                }
+                Gl.glEnd();
+            }
+
+            // Sec
+            Gl.glLoadIdentity();
+            Gl.glViewport(this.simpleOpenGlControl1.Size.Width / 3 * 2, this.simpleOpenGlControl1.Size.Height / 2, this.simpleOpenGlControl1.Size.Width / 3, this.simpleOpenGlControl1.Size.Height / 2);
+            Glu.gluOrtho2D(count * -Math.PI, count * Math.PI, -2.0f, 2.0f);
+            Gl.glColor3d(0.0, 0.0, 0.0); //black
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = count * -2 * Math.PI; x <= count * 2 * Math.PI; x += 0.005)
+                {
+                    double fx = 1 / Math.Cos(x);
+                    if (Math.Abs(Math.Tan(x) - Math.Tan(x - 0.01)) > 1)
+                    {
+                        Gl.glEnd();
+                        Gl.glVertex2d(x, fx);
+                        Gl.glBegin(Gl.GL_LINE_STRIP);
+                    }
+                    else
+                    {
+                        Gl.glVertex2d(x, fx);
+                    }
+                }
+            Gl.glEnd();
+
+            // CSC
+            for (int i = 0; i < count; i++)
+            {
+                Gl.glLoadIdentity();
+                Gl.glViewport(this.simpleOpenGlControl1.Size.Width / 3 * 2 + this.simpleOpenGlControl1.Size.Width / 3 / count * i, 0, this.simpleOpenGlControl1.Size.Width / 3 / count, this.simpleOpenGlControl1.Size.Height / 2);
+                Glu.gluOrtho2D(-Math.PI, Math.PI, -2.0f, 2.0f);
+                Gl.glColor3d(0.0, 0.0, 0.0); //black
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double x = -Math.PI; x < 0; x += 0.005)
+                {
+                    double fx = 1 / Math.Sin(x);
+                    Gl.glVertex2d(x, fx);
+                }
+                for (double x = 0; x < Math.PI; x += 0.005)
+                {
+                    double fx = 1 / Math.Sin(x);
+                    Gl.glVertex2d(x, fx);
+                }
+                Gl.glEnd();
+            }
+
+        }
+
+        private void simpleOpenGlControl1_Resize(object sender, EventArgs e)
+        {
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0, this.simpleOpenGlControl1.Size.Width, 0.0, this.simpleOpenGlControl1.Size.Height);
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+        }
+
+        private void btnCountUp_Click(object sender, EventArgs e)
+        {
+            if (count < 5)
+            {
+                count++;
+                tbxCount.Text = count.ToString();
+            }
+        }
+
+        private void btnCountDown_Click(object sender, EventArgs e)
+        {
+            if (count > 1)
+            {
+                count--;
+                tbxCount.Text = count.ToString();
+            }
+        }
+
+        private void tbxCount_TextChanged(object sender, EventArgs e)
+        {
+            Repaint();
+        }
+
+
+    }
+}
